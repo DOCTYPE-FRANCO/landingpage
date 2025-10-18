b67087d29eadff7df5e1631e04689b22e8e18ea9
Franco <ufrancis186@gmail.com>
2025-10-19 00:10:14 +0100

    whole lot

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { motion } from "framer-motion";
 function Hero(){
     return(
-        <div className="flex flex-col gap-3 items-center justify-center fixed top-0 h-[300px] w-full bg-purple-800">
-            <p className="font-bold text-white text-5xl max-w-[35vw] text-center">Book Sonia for Your Next Project</p>
-            <p className="font-thin text-white text-2xl max-w-[35vw] text-center">Multifacet Content Creator with 600k+ followers across all platforms</p>
+        <div className="flex flex-col gap-5 items-center justify-center relative top-0 h-[300px] w-full bg-purple-950">
+            <motion.p 
+                initial={{opacity:0, x:-100}}
+                animate={{opacity:1, x:0}}
+                transition={{duration: 2}}
+                className="font-bold text-white md:text-5xl max-w-[40vw] text-center"
+            >
+                Book Sonia for Your Next Project
+            </motion.p>
+            <motion.p
+                initial={{opacity:0, x:100}}
+                animate={{opacity:1, x:0}}
+                transition={{duration: 2}}
+                className="font-thin text-white text-2xl max-w-[35vw] text-center"
+            >
+                Multifacet Creative with <span className="font-bold">600k+</span> followers across all platforms
+            </motion.p>
+
+            <a className="font-bold text-white border-white border rounded-full p-3 hover:scale-105 transition-all duration-500">Book Now</a>
         </div>
     );
 }
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,10 +1,62 @@
 import React from "react";
 import Hero from "./Hero";
+import Sonia from "../assets/Sonia.jpg"
+import Ig from "../assets/igg.png"
+import Tiktok from "../assets/tt.png"
+import Youtube from "../assets/yt.png"
+
+
 
 function Homepage(){
     return(
-        <div>
+        <div className="flex flex-col">
             <Hero />
+
+            <div className="mt-7 flex flex-row md:gap-20 justify-center ">
+                <div className="flex justify-centerw-[100px] h-[100px] rounded-full overflow-hidden">
+                    <img src={Sonia} loading="lazy" className="w-full h-full object-top"/>
+                </div>
+
+                <div>
+                    <p className="text-2xl font-extrabold text-left mb-4">Dike Sonia</p>
+                    <p className="font-semibold text-left max-w-[450px]">Hi, i'm Dike Sonia aka <span className="font-bold">@soniatherotationqueen</span>.<br/> A versatile content creator blending creativity, strategy, and authenticity to connect with a vibrant and growing audience.</p>
+                </div>
+            </div>
+
+            <div className="flex flex-row gap-10 justify-center mt-10">
+                <div className=" flex flex-col gap-3 justify-center items-center w-[200px] shadow-2xl shadow-gray-400">
+                    <p className="font-bold text-2xl">560K +</p>
+
+                    <div className="flex flex-row justify-center items-center">
+                        <div className="w-[50px] h-[50px] rounded-full overflow-hidden">
+                            <img src={Tiktok}/>
+                        </div>
+                        <p className="font-bold">Tiktok</p>
+                    </div>
+                </div>
+
+                <div className="flex flex-col gap-3 justify-center items-center w-[200px] shadow-2xl shadow-gray-400">
+                    <p className="font-bold text-2xl">48K +</p>
+
+                    <div className="flex flex-row justify-center items-center">
+                        <div className="w-[50px] h-[50px] rounded-full overflow-hidden">
+                            <img src={Ig}/>
+                        </div>
+                        <p className="font-bold">Instagram</p>
+                    </div>
+                </div>
+
+                <div className="flex flex-col gap-3 justify-center items-center w-[200px] shadow-2xl shadow-gray-400">
+                    
+                    <p className="font-bold text-2xl">10K +</p>
+                    <div className="flex flex-row justify-center items-center">
+                        <div className="w-[50px] h-[50px] rounded-full overflow-hidden">
+                            <img src={Youtube}/>
+                        </div>
+                        <p className="font-bold">Youtube</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
