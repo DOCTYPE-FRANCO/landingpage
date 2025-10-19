c2b27dedcb28638815767cbab0735b55dc5ee96c
Franco <ufrancis186@gmail.com>
2025-10-19 21:10:56 +0100

    fix

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -32,14 +32,14 @@ function Homepage(){
         <div className="flex flex-col">
             <Hero />
 
-            <div className="mt-7 flex items-center md:items-start flex-col md:flex-row md:gap-20 justify-center ">
+            <div className="mt-7 flex items-center md:items-start flex-col md:flex-row md:gap-20 justify-center mx-auto">
                 <div className="flex justify-center w-[100px] h-[100px] rounded-full overflow-hidden hover:scale-105 transition-transform duration-300 ">
                     <img src={Sonia} loading="lazy" className="w-full h-full object-top"/>
                 </div>
 
                 <div>
                     <p className="text-2xl font-extrabold text-left mb-4">Dike Sonia</p>
-                    <p className="font-semibold text-left max-w-[380px] md:max-w-[450px]">Hi, i'm Dike Sonia aka <span className="font-bold">@soniatherotationqueen</span>.<br/> A versatile content creator blending creativity, strategy, and authenticity to connect with a vibrant and growing audience.</p>
+                    <p className="font-semibold text-left max-w-[400px] md:max-w-[450px]">Hi, i'm Dike Sonia aka <span className="font-bold">@soniatherotationqueen</span>.<br/> A versatile content creator blending creativity, strategy, and authenticity to connect with a vibrant and growing audience.</p>
                 </div>
             </div>
 
