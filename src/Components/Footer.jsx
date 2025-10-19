40bb5426332757a00d739b59ade059aa68fe666a
Franco <ufrancis186@gmail.com>
2025-10-19 15:46:08 +0100

    changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,15 +10,15 @@ function Footer(){
             <div className="flex flex-row gap-3 justify-center">
             
                     <div>
-                        <a href="https://www.linkedin.com/in/uchenna-emeh-9b5833356?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"><img src={Tiktok} className="w-[30px] hover:scale-125 transition-all duration-500 rounded-full"/></a>
+                        <a href="https://www.tiktok.com/@soniatherotationqueen?_t=ZS-90gMLsJ5p2n&_r=1"><img src={Tiktok} className="w-[30px] hover:scale-125 transition-all duration-500 rounded-full"/></a>
                     </div>
                            
                     <div>
-                        <a href="https://wa.me/2348120362926?text=Hello%20i%20got%20your%20number%20from%20your%20portfolio"><img src={Ig} className="w-[30px] hover:scale-125 transition-all duration-500 rounded-2xl"/></a>
+                        <a href="https://www.instagram.com/soniafashionkyle?igsh=bnh4Z2VveDgwbWF3"><img src={Ig} className="w-[30px] hover:scale-125 transition-all duration-500 rounded-2xl"/></a>
                     </div>
                         
                     <div>
-                        <a href="https://x.com/doctype_franco?s=21"><img src={Youtube} className="w-[30px] hover:scale-125 transition-all duration-500 rounded-md"/></a> 
+                        <a href="https://youtube.com"><img src={Youtube} className="w-[30px] hover:scale-125 transition-all duration-500 rounded-md"/></a> 
                     </div>
             
             </div>
